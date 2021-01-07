2be881940eb5ae57814fa7c9beb9835ce4bae6be
PelleLindroth <pellelindroth@gmail.com>
2021-01-07 21:04:10 +0100

    Style CardStack and render list of cards

--- e-wallet/src/components/CardStack.vue
+++ e-wallet/src/components/CardStack.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="card-stack-wrapper">
-    CARD STACK COMPONENT
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-  </div>
+  <section class="card-stack">
+    <Card v-for="card of cards" :key="card.id" :userCard="card" />
+  </section>
 </template>
 
 <script>
@@ -14,7 +10,18 @@ export default {
   components: {
     Card,
   },
+  computed: {
+    cards() {
+      return this.$root.cards
+    },
+  },
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.card-stack {
+  margin: 2rem 0 12rem;
+  display: grid;
+  grid-auto-rows: 4rem;
+}
+</style>
