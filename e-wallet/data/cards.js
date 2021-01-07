f35d50faf1b24f1213f94c0d6ee3c7f274ac009f
PelleLindroth <pellelindroth@gmail.com>
2021-01-07 20:10:14 +0100

    Refactor data folder to project root

---
+++ e-wallet/data/cards.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    id: "432984728397",
+    holder: "Tjalle bergqvist",
+    vendor: "ninja",
+    number: "1987487571938948",
+    validMonth: "12",
+    validYear: "25"
+  },
+  {
+    id: "432984728397",
+    holder: "Tjalle bergqvist",
+    vendor: "bitcoin",
+    number: "123750980987345",
+    validMonth: "12",
+    validYear: "25"
+  },
+  {
+    id: "432984728397",
+    holder: "Tjalle bergqvist",
+    vendor: "blockchain",
+    number: "12971197319751981",
+    validMonth: "12",
+    validYear: "25"
+  },
+  {
+    id: "432984728397",
+    holder: "Tjalle bergqvist",
+    vendor: "evil",
+    number: "0089674598496758",
+    validMonth: "12",
+    validYear: "25"
+  }
+]
--- e-wallet/src/main.js
+++ e-wallet/src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import cards from '../data/cards'
 
 Vue.config.productionTip = false
 
 new Vue({
+  data() {
+    return {
+      cards: cards
+    }
+  },
   router,
   render: h => h(App)
 }).$mount('#app')
