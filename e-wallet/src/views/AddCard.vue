287531dc53f09c973221f8e1bf15cb3c1cee004d
PelleLindroth <pellelindroth@gmail.com>
2021-01-08 16:57:36 +0100

    Add input validation

--- e-wallet/src/views/AddCard.vue
+++ e-wallet/src/views/AddCard.vue
@@ -9,6 +9,11 @@
       @editVendor="editVendor"
       @editYear="editYear"
     />
+    <div class="error-container">
+      <p v-show="error" class="error-message">
+        {{ errorMessage }}
+      </p>
+    </div>
     <button @click="addCard">Add Card</button>
   </main>
 </template>
@@ -26,6 +31,8 @@ export default {
   },
   data() {
     return {
+      error: false,
+      errorMessage: '',
       newCard: {
         holder: '',
         vendor: 'blank',
@@ -37,6 +44,8 @@ export default {
   },
   methods: {
     addCard() {
+      if (!this.validateInput()) return
+
       this.newCard.id = uid(12)
       this.$root.addCard(this.newCard)
       this.$router.push('/')
@@ -56,6 +65,48 @@ export default {
     editYear(year) {
       this.newCard.validYear = year
     },
+    validateInput() {
+      this.error = false
+      if (this.newCard.number.length < 16) {
+        this.errorMessage = 'Card number must be 16 digits'
+        this.error = true
+        return false
+      } else if (Number.isNaN(+this.newCard.number)) {
+        this.errorMessage = 'Card number can only contain digits'
+        this.error = true
+        return false
+      } else if (!this.newCard.holder.length) {
+        this.errorMessage = 'Please submit card holder'
+        this.error = true
+        return false
+      } else if (Number.isNaN(+this.newCard.validMonth)) {
+        this.errorMessage = 'Please enter a valid month'
+        this.error = true
+        return false
+      } else if (Number.isNaN(+this.newCard.validYear)) {
+        this.errorMessage = 'Please enter a valid year'
+        this.error = true
+        return false
+      } else if (this.newCard.vendor == 'blank') {
+        this.errorMessage = 'Please submit card vendor'
+        this.error = true
+        return false
+      } else {
+        return true
+      }
+    },
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.error-container {
+  min-height: 1rem;
+
+  p {
+    color: red;
+    font-size: 0.8rem;
+    text-transform: uppercase;
+  }
+}
+</style>
